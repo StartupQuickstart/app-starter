b5072aadf68ae6f36b4a445757a90ccc70503942
Thomas Boles <tcboles@me.com>
2022-06-26 14:58:23 -0400

    Updates packages

--- src/client/src/setupProxy.js
+++ src/client/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     '/api',
     proxy.createProxyMiddleware({
-      target: 'http://localhost:3501',
+      target: 'http://localhost:3001',
       secure: false,
     })
   );
